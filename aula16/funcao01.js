9aeb8ca2cb30d3b264df2d00ff9decc6bed9de1f
nivaldoCouto <nivaldo_couto@hotmail.com>
2021-05-18 23:20:27 -0300

    add aula16-funcao01, 02, 03, 04, 05.js

---
+++ aula16/funcao01.js
@@ -0,0 +1,15 @@
+function parImpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+// mostrar resultdo modo simplificado
+
+console.log(parImpar(12))
+
+
+// normal, mostrar resultado com uma variavel
+// let res = parImpar(5)
+// console.log(res)

816183098f84fe3142be14be8b6bbd3cc6e7e669
nivaldoCouto <nivaldo_couto@hotmail.com>
2021-05-18 23:08:23 -0300

    add aula13-ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+//do wile
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+// wile
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+=========================
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/
